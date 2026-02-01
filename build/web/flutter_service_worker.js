d6b60afb6a2f58523d4144322b3cb3c040ef0ab1
Catalyst-101 <skyisblack95@gmail.com>
2026-02-01 13:01:38 +0500

    add2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b27800dc16b1adf366340f54301ab240
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "7b93365e54339aaed592b63b2e12e87e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ca1c71c0a0826236272c1b0d3c2666bd",
+"flutter_bootstrap.js": "a36a95afd6cd4fdbe4c5d7bbb351d740",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
